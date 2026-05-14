e7a2dce9e807557fbb96939f0869edf225fe00e7
devopsannu <devopsannu@gmail.com>
2026-05-15 02:25:08 +0530

    microui build

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 
 const config = {
-  GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://default-get-tasks-url',
-  DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://default-delete-task-url',
-  CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://default-create-task-url',
+  GET_TASKS_API_BASE_URL: process.env.REACT_APP_GET_TASKS_API_BASE_URL || 'http://20.207.119.187/api/get',
+  DELETE_TASK_API_BASE_URL: process.env.REACT_APP_DELETE_TASK_API_BASE_URL || 'http://20.207.119.187/api/delete',
+  CREATE_TASK_API_BASE_URL: process.env.REACT_APP_CREATE_TASK_API_BASE_URL || 'http://20.207.119.187/api/add',
 };
 
 export default config;
